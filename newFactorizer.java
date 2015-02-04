7d9e133d77f0ed81eb7ee2abab8fdbce89d931b4
Brendan Liu <liuyuxin@amazon.com>
2015-02-04 13:07:23 -0800

    Applied Java Formmater

--- newFactorizer.java
+++ newFactorizer.java
@@ -1,20 +1,20 @@
 public class Factorizer {
-    
-    public static void main(String [] args){
-        int n=Integer.parseInt(args[0]);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
         calculatePrimeFactors(n);
     }
-    
-    public static void calculatePrimeFactors (int n){
-        for(int i=2; i<=(Math.sqrt(n));i++){
-            System.out.println("i = "+i+ " number= "+n);
-            while(n%i==0){
-                n=n/i;
-                System.out.println("factor: "+i);
+
+    public static void calculatePrimeFactors(int n) {
+        for (int i = 2; i <= (Math.sqrt(n)); i++) {
+            System.out.println("i = " + i + " number= " + n);
+            while (n % i == 0) {
+                n = n / i;
+                System.out.println("factor: " + i);
             }
         }
-        if(n>1){
-            System.out.println("factor: "+n);
+        if (n > 1) {
+            System.out.println("factor: " + n);
         }
         System.out.println();
     }
